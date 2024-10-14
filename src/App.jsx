a41782eb8a42aedbc580f1bf1c01d1a0c31642e7
Nahanur-islam <nahnanurism71@gmail.com>
2024-10-14 11:59:31 +0600

    blog part completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
+import BookMarks from './Components/BookMarks/BookMarks'
 import Header from './Components/Headers/Header'
 
 function App() {
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className='md:flex gap-2 '>
+        <Blogs></Blogs>
+        <BookMarks></BookMarks>
+      </div>
     </>
   )
 }
--- src/Components/Headers/Header.jsx
+++ src/Components/Headers/Header.jsx
@@ -3,10 +3,10 @@ import profile from '../../assets/image/profile.png'
 
 const Header = () => {
     return (
-        <div className='flex justify-between items-center p-4 mx-4 border-b-2'>
+        <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
             <h1 className='text-4xl font-bold'>Knoelage Cafe</h1>
             <img src={ profile} alt="" />
-        </div>
+        </header>
     );
 };
 
